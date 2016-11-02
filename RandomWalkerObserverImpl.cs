f4e1ee6634f23a55fa0f25b76a3b998a46e46c11
Ivy Chang <ivycwq@icloud.com>
2016-11-02 13:08:59 -0700

    adding Name properties for obs

--- RandomWalkerObserverImpl.cs
+++ RandomWalkerObserverImpl.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace RandomWalker {
   public class RandomWalkerObserverImpl : IRandomWalkObserver{
+    
+    public string ObsName {get; set;}
+    public RandomWalkerObserverImpl(string name) {
+      ObsName = name;
+    }
     public void Moved(RandomWalker subject, string direction) {
-      Console.WriteLine("Subject {0} moved a step to the {1}", subject.Name, direction);
+      Console.WriteLine("{0}: Subject {0} moved a step to the {1}", ObsName, subject.SubjectName, direction);
     }
   }
 }
