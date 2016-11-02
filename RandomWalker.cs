b7fa66c2a7af00603fde1af9660416ab73988766
Ivy Chang <ivycwq@icloud.com>
2016-11-02 12:51:01 -0700

    adding randomwalker class

---
+++ RandomWalker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomWalker {
+    public class RandomWalker {
+        Random r = new Random();
+        private List<IRandomWalkObserver> observers = new List<IRandomWalkObserver>();
+        public void AddObserver(IRandomWalkObserver obs) {
+            observers.Add(obs);
+        }
+        public void RemoveObserver(IRandomWalkObserver obs) {
+            observers.Remove(obs);
+        }
+        public void Move() {
+            foreach(IRandomWalkObserver obs in observers)
+                obs.Moved(this, GetDirection());
+        }
+        public string GetDirection() {
+            double d = r.NextDouble();
+            if (d < 0.5) return "left";
+            else return "right";
+        }
+    }
+}
